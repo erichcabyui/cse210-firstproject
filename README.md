ec89532c2120ad45d8c429aa3bc311dbda15483b
erichcabyui <carterer@byui.edu>
2022-09-14 18:34:34 -0700

    First commit with updated readme and new version info test code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cse210-firstproject
 A simple test project to learn about git
+
+This is my first change that i have made on my local machine.
---
+++ test.py
@@ -0,0 +1,7 @@
+# Python program to print the version information
+
+# importing the module
+import sys
+
+# printing version info
+print("Python version: ", sys.version)
